fc755f6406f58ab5894623645f04aa7c9d89012a
amir260101~ <amir26hossin@gmail.com>
2023-05-19 11:01:36 +0600

    view details done

--- index.js
+++ index.js
@@ -36,6 +36,13 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/toy/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await toysCollection.findOne(query);
+      res.send(result);
+    })
+
     app.delete('/allToys/:id', async (req, res) => {
       const id = req.params.id;
       const query = {_id: new ObjectId(id)};
